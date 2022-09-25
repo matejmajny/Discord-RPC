f9a4706bc6eb7a288c0d7d330c7a32f68326adc9
Matthew <88656901+matejmajny@users.noreply.github.com>
2022-09-25 13:33:53 +0200

    Initial upload

---
+++ index.js
@@ -0,0 +1,27 @@
+const RPC = require("discord-rpc");
+const rpc = new RPC.Client({ transport: 'ipc' });
+const config = require('./config.json');
+
+rpc.on("ready", () => {
+    console.log("Rich Presence set!")
+    rpc.setActivity({
+        largeImageKey: config.largeImage,
+        largeImageText: config.largeImageText,
+        smallImageKey: config.smallImage,
+        smallImageText: config.smallImage,
+        state: config.state,
+        details: config.details,
+        startTimestamp: new Date(),
+
+        buttons: [{
+            label: config.button1,
+            url: config.buttonURL1
+        }, {
+            label: config.button2,
+            url: config.buttonURL2
+        }]
+    })
+})
+
+rpc.login({ clientId: config.clientID })
+console.log("Logged as: " + config.clientID)
---
+++ run.bat
@@ -0,0 +1,11 @@
+@echo off
+title Discord RPC
+set /p i=Install [I] or Run [R]
+
+if %i% == "R":
+    node index.js
+
+else:
+    npm install
+
+PAUSE
